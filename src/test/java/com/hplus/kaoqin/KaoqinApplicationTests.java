24f001bbd12c450eb333d56bb2bf6535f5aabba1
liujiang <2449767269@qq.com>
2020-12-23 10:34:06 +0800

    2020/12/23(jacky):junit报错修改

--- src/test/java/com/hplus/kaoqin/KaoqinApplicationTests.java
+++ src/test/java/com/hplus/kaoqin/KaoqinApplicationTests.java
@@ -1,13 +1,16 @@
 package com.hplus.kaoqin;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @SpringBootTest
-class KaoqinApplicationTests {
+@RunWith(SpringJUnit4ClassRunner.class)
+public class KaoqinApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
